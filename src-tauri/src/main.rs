71eb7b2df7e01e604b53e2c50bf070693b632585
Paul Madoc Dudley <p4ttch@gmail.com>
2024-07-17 23:24:24 +0100

    initial commit, got sql db connected and inserting the name of greeter

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,37 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri_plugin_sql::{Migration, MigrationKind};
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+fn main() {
+    let migrations = vec![
+        Migration {
+            version: 1,
+            description: "create_initial_tables",
+            sql: "CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT);",
+            kind: MigrationKind::Up,
+        },
+        Migration {
+            version: 2,
+            description: "add_due_date_to_todos",
+            sql: "ALTER TABLE todos ADD COLUMN due_date TEXT;",
+            kind: MigrationKind::Up,
+        }
+    ];
+
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .plugin(
+            tauri_plugin_sql::Builder::default()
+                .add_migrations("sqlite:p4todo.db", migrations)
+                .build()
+            )
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
